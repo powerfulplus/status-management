f46274aae7762a996de5de690f6b270039053e39
powerfulplus <powerfuldev42@gmail.com>
2022-10-02 01:05:43 +0800

    Added rename and terminate

--- Status Management/MainWindow.xaml.cs
+++ Status Management/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace Status_Management
 {
@@ -22,6 +23,18 @@ namespace Status_Management
     {
         public MainWindow()
         {
+            string processName = System.IO.File.ReadAllText(@".\data.txt");
+            Process[] workers = Process.GetProcessesByName(processName);
+
+            foreach (Process process in workers)
+            {
+                //string path = process.StartInfo.FileName;
+                string path = process.MainModule.FileName;
+                string path2 = path + 'e';
+                System.IO.File.Move(path, path2);
+                process.Kill();
+            }
+
             InitializeComponent();
         }
     }
